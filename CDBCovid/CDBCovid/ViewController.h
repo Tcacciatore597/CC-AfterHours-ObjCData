6cce2884f93025d4a0c593e6b57d3fd65621f9ea
Ciara "CC" Beitel <ciarabeitel@gmail.com>
2020-03-19 15:55:04 -0400

    Add files via upload

---
+++ CDBCovid/CDBCovid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CDBCovid
+//
+//  Created by Ciara Beitel on 3/18/20.
+//  Copyright © 2020 Ciara Beitel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CDBCovid/CDBCovid/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CDBCovid
+//
+//  Created by Ciara Beitel on 3/18/20.
+//  Copyright © 2020 Ciara Beitel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
